708421e16993781d8fa0f65987065de0bc1ff76e
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-12 22:00:14 +0300

    feat: добавлены DTO

---
+++ src/main/java/com/example/openschool2/dto/TaskRequestDto.java
@@ -0,0 +1,22 @@
+package com.example.openschool2.dto;
+
+import com.example.openschool2.model.Status;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@Data
+@Accessors(chain = true)
+public class TaskRequestDto {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime dueDate;
+
+    private Status status;
+
+}
