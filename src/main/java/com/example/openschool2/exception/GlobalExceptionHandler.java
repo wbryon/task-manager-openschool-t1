3787ddff09ce2210bb239364da52f49b4072624b
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-14 18:19:34 +0300

    feat: добавлена обработка исключения BadRequestException

--- src/main/java/com/example/openschool2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/openschool2/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.openschool2.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         FieldError error = e.getFieldError();
@@ -22,6 +24,18 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException e) {
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequestException(BadRequestException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return new ErrorResponse(e.getMethod() + "-method not allowed on this endpoint");
     }
 
     @ExceptionHandler
--- src/main/java/com/example/openschool2/exception/TaskAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.openschool2.exception;
-
-public class TaskAlreadyExistsException extends RuntimeException {
-    public TaskAlreadyExistsException(String message) {
-        super(message);
-    }
-}
