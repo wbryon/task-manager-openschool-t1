4267048a1176b860eb6e8e144cbe97436691e7bd
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-14 19:27:05 +0300

    feat: добавлен класс TaskControllerTest для тестирования методов

---
+++ src/test/java/com/example/openschool2/controller/TaskControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.openschool2.controller;
+
+import com.example.openschool2.service.TaskService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TaskController.class)
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskService taskService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
