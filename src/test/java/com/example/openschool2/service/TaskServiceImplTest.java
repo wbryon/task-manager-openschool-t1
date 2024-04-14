35aae835d52d385a0171288ed0eb66df1563a7a4
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-14 19:56:10 +0300

    feat: добавлен класс TaskServiceImplTest для тестирования методов

---
+++ src/test/java/com/example/openschool2/service/TaskServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.example.openschool2.service;
+
+import com.example.openschool2.repository.TaskRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceImplTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskServiceImpl taskService;
+
+}
